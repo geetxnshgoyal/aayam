8b4c8904eb1101026870ed6208d288201bc875c7
Geetansh Goyal <goyalgeetansh@gmail.com>
2026-03-06 13:03:27 +0530

    updated design

--- lib/site.ts
+++ lib/site.ts
@@ -1,13 +1,18 @@
-export const PRIMARY_DOMAIN = "aayam.xyz";
+export const PRIMARY_DOMAIN = "aayamfest.com";
 export const PRIMARY_SITE_URL = `https://${PRIMARY_DOMAIN}`;
 
 /** Logo used for favicon, OG, Twitter, and all indexing/social images */
 export const SITE_LOGO_PATH = "/images/logo_clean.png";
 
+/** Other configured domains (for sameAs / SEO); primary is aayamfest.com */
 export const DOMAIN_ALIASES = [
+  "www.aayamfest.com",
+  "aayam.xyz",
+  "www.aayam.xyz",
   "aayamfest.xyz",
+  "www.aayamfest.xyz",
   "aayamtechfest.xyz",
-  "aayamfest.com",
+  "www.aayamtechfest.xyz",
 ];
 
 export const ALL_SITE_URLS = [PRIMARY_SITE_URL, ...DOMAIN_ALIASES.map((domain) => `https://${domain}`)];
